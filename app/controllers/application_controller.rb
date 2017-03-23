c703036529fc510b207b455b5737782d55e4e29a
PietroSharks <pietro.c191@gmail.com>
2017-03-22 22:04:03 -0400

    Detalles de Web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
     include HTTParty
   
-    base_uri 'http://192.168.2.8:3000/' #'https://alice-intranet-kevingn.c9users.io/'
+    base_uri 'http://192.168.1.118:3001/' #'https://alice-intranet-kevingn.c9users.io/'
 
   
   	def Respond_notice(response)
